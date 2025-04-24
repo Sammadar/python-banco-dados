fba750519490b23d5eb8a2e2af8e40363894651e
Sammadar dos Santos <sammadar@outlook.com.br>
2025-04-23 22:35:41 -0300

    Adicionado criação automágica do banco de dados ao iniicar a aplicação

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import os
+ 
 import questionary
-
-from src.telas.cliente_telas import executar_cliente
+ 
+from src.database.criar_banco_dados import criar_database
 from src.telas.produto_tela import executar_produto
-
+ 
+ 
 if __name__ == "__main__":
-    os.system("cls") # Limpar a tela
-    opcoes = ["Clientes", "Produtos", "Sair"] # Lista de opções para o usuário
-    opcao_desejada = ""
-    while opcao_desejada != "Sair":
+     os.system("cls") # Limpar a tela
+     criar_database()
+     opcoes = ["Clientes", "Produtos", "Sair"] # Lista de opções para o usuário
+     opcao_desejada = ""
+     while opcao_desejada != "Sair":
         opcao_desejada = questionary.select("Escolha o menu desejado", opcoes).ask()
-
+ 
         if opcao_desejada == "Produtos":
             executar_produto()
-        elif  opcao_desejada == "Clientes":
-            executar_cliente()
